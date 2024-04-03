ba937b32f05969677d53432a245af3074f6c154a
Tabarin01 <sandrodesilvestro@gmail.com>
2024-04-03 19:05:30 +0200

    Aiuto
    Co-authored-by: Kamer_Hasa <KamerHasa@users.noreply.github.com>
    Co-authored-by: Lykaon <Lyka0n@users.noreply.github.com>
    Co-authored-by: Simone Michelon <SimoneMichelon@users.noreply.github.com>

--- SocialNetworkFE/src/app/components/confirm-user-delete/confirm-user-delete.component.ts
+++ SocialNetworkFE/src/app/components/confirm-user-delete/confirm-user-delete.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute } from "@angular/router";
 import { UserService } from "src/app/services/Users/user.service";
 import { FeedbackComponent } from "../feedback/feedback.component";
+import { AuthService } from "src/app/services/Auth/auth.service";
 
 @Component({
   selector: "app-confirm-user-delete",
@@ -13,18 +14,22 @@ export class ConfirmUserDeleteComponent {
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
+    private authService: AuthService,
     public dialog: MatDialog
   ) {}
 
   id: any;
+  userAuth: any;
 
   ngOnInit(): void {
+    this.authService.getUserProfile().subscribe({
+      next: (data) => (this.userAuth = data),
+    });
+
     this.id = this.route.snapshot.paramMap.get("id");
-    console.log(this.id);
   }
 
   onClick() {
-    
     this.userService.deleteUser(this.id).subscribe({
       next: () => {
         console.log("User eliminato con successo.");
@@ -33,8 +38,31 @@ export class ConfirmUserDeleteComponent {
         console.error("Errore durante l'eliminazione del post:", error);
       },
     });
-    this.openDialog();
+
+    console.log(this.id + " == " + this.userAuth.id);
+
+    if (this.id == this.userAuth.id) {
+      this.authService.logout();
+    } else {
+      this.openDialog();
+    }
   }
+
+  /**
+ * 
+ * ngOnInit(): void {
+    this.authService.getUserProfile().subscribe({
+      next: (data) => (this.userAuth = data),
+    });
+
+    this.postService.getPosts().subscribe();
+    this.postService.postSubject.subscribe((state) => {
+      this.posts = this.sortPostsById(state.posts);
+    });
+  }
+ * 
+ * 
+ */
 
   openDialog(): void {
     const dialogRef = this.dialog.open(FeedbackComponent);
